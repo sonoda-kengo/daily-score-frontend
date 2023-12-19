0933a28d4a6ad9bbdec8191366e2e082b80c4c48
sonoda-kengo <wsho.magn07@gmail.com>
2023-12-19 20:21:42 +0900

    add borderBottom at todo-line

--- components/todo-line.tsx
+++ components/todo-line.tsx
@@ -13,14 +13,16 @@ export const TodoLine = ({ todo }: { todo: ITodo }) => {
           }}
         />
       </Grid>
-      <Grid item xs={6}>
-        <Typography>{todo.title}</Typography>
-      </Grid>
-      <Grid item xs={3}>
-        <Typography>{todo.category}</Typography>
-      </Grid>
-      <Grid item xs={2}>
-        <Typography>{todo.date}</Typography>
+      <Grid container xs={11} borderBottom={1} alignItems='center'>
+        <Grid item xs={6}>
+          <Typography ml={1}>{todo.title}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography>{todo.category}</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography>{todo.date}</Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
